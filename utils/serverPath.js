2882c739328756ab0dcd9db5c344e9182469fb5e
Mohammad Mosa Agha Jahanmal <iamceayber@gmail.com>
2025-11-20 16:08:29 +0430

    tiny changes for the server

--- utils/serverPath.js
+++ utils/serverPath.js
@@ -1,2 +1,2 @@
-// export default (path) => (`http://192.168.237.25:8080${path}`); 
+// export default (path) => (`http://192.168.43.238:8080${path}`); 
 export default (path) => (`https://bloodbank.jahanmal.xyz${path}`); 
