979542d49072a023160e1a5ab170ed2a073e4068
Mohammad Mosa Agha Jahanmal <iamceayber@gmail.com>
2025-11-28 20:15:23 +0430

    simple

--- app/(drawer)/_layout.js
+++ app/(drawer)/_layout.js
@@ -145,7 +145,6 @@ function CustomDrawerContent(props) {
       const result = await response.json();
 
       if (result.status === 'success') {
-        // Update user context with deletion request status
           setAuth(prev => ({
             ...prev,
             user: {...prev.user, deletionRequest: true }
