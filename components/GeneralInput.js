34614230a6c65f175e07092babf46e2e604dd718
Mohammad Mosa Agha Jahanmal <iamceayber@gmail.com>
2026-01-09 21:39:31 +0430

    .

--- components/GeneralInput.js
+++ components/GeneralInput.js
@@ -56,6 +56,7 @@ const Input = function Input({
           writingDirection={writingDirection}
           autoCorrect={false}
           spellCheck={false}
+          // Multiline props
           multiline={multiline}
           numberOfLines={multiline ? numberOfLines : undefined}
           textAlignVertical={multiline ? textAlignVertical : 'center'}
