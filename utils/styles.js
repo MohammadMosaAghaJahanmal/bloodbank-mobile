7e3db8bd45a7ab1cddf9acdea8facee25e8e91d5
Mohammad Mosa Agha Jahanmal <iamceayber@gmail.com>
2026-02-05 15:01:35 +0430

    contact

--- utils/styles.js
+++ utils/styles.js
@@ -2452,6 +2452,7 @@ about: (writingDirection) => ({
     teacherBio: { color: COLORS.textLight, marginTop: 6, lineHeight: 20, fontSize: 13 },
 
     /* Contact */
+    
     contactItem: { flexDirection: 'row', alignItems: 'center', paddingVertical: 12 },
     contactIcon: {
       width: 36,
